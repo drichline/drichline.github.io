8cd279e0d608a7fa8e46fab95c17cb08aff12a54
Dakota Richline <drichline@protonmail.com>
2024-07-17 13:23:22 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 hugo
-rm -r /mnt/slab/containers/proxy/perilune
-mv public /mnt/slab/containers/proxy/perilune
+rm -r /mnt/slab/containers/proxy/perilune/*
+mv public/* /mnt/slab/containers/proxy/perilune
